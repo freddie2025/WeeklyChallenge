d89cc9b39f374c6efbc3825439c44699bf4ca124
Fred James <fredjsam@live.co.uk>
2020-01-31 19:48:25 +0000

    Completed Foreach Challenge

---
+++ WCH-01/ForeachChallenge/ConsoleUI/PersonModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUI
+{
+	class PersonModel
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+	}
+}
---
+++ WCH-01/ForeachChallenge/ConsoleUI/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleUI
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var people = GetPeopleStrings();
+
+			foreach (var person in people)
+			{
+				Console.WriteLine($"Hello {person}");
+			}
+
+			Console.ReadLine();
+
+			foreach (var personModel in GetPersonModels())
+			{
+				Console.WriteLine($"Hello {personModel.FirstName} {personModel.LastName}");
+			}
+
+			Console.ReadLine();
+		}
+
+		private static List<PersonModel> GetPersonModels() => 
+			new List<PersonModel>()
+			{ 
+				new PersonModel { FirstName="Tim", LastName="Corey" },
+				new PersonModel { FirstName="Bill", LastName="McCoy" },
+				new PersonModel { FirstName="Mary", LastName="Jones" },
+				new PersonModel { FirstName="Sue", LastName="Smith" },
+			};
+
+		private static List<string> GetPeopleStrings() =>
+			new List<string>()
+			{
+				"John",
+				"Mary",
+				"Sue",
+				"Greg",
+				"Yolanda",
+				"Jose",
+				"Will",
+			};
+	}
+}
