095fe60a614bcf8b481b1d25c60b535d4cde4054
Fred James <fredjsam@live.co.uk>
2020-02-02 14:44:37 +0000

    Setting up Error Handling Challenge

---
+++ WCH-03/ErrorHandlingChallengeStarterCode/ConsoleUI/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoLibrary;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PaymentProcessor paymentProcessor = new PaymentProcessor();
+            for (int i = 0; i <= 10; i++)
+            {
+                var result = paymentProcessor.MakePayment($"Demo{ i }", i);
+
+                Console.WriteLine(result.TransactionAmount);
+            }
+            Console.ReadLine();
+        }
+    }
+}
