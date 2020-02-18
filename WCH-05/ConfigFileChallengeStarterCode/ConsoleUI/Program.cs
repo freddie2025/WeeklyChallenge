6da1e1d98fc6f95f420b4879db6e24dc7f65e47d
Fred James <fredjsam@live.co.uk>
2020-02-18 09:31:32 +0000

    Config File Challenge Complete

--- WCH-05/ConfigFileChallengeStarterCode/ConsoleUI/Program.cs
+++ WCH-05/ConfigFileChallengeStarterCode/ConsoleUI/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
 
 namespace ConsoleUI
 {
@@ -10,6 +7,59 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            string serverIP = ConfigurationManager.AppSettings["ServerIPAddress"];
+            string defaultConnection = ConfigurationManager.ConnectionStrings["Default"].ConnectionString;
+
+            Console.WriteLine(serverIP);
+            Console.WriteLine(defaultConnection);
+            Console.WriteLine();
+
+            var appSettings = ConfigurationManager.AppSettings;
+
+            foreach (var key in appSettings.AllKeys)
+            {
+                Console.WriteLine($"Key: { key } Value: { appSettings[key] }");
+            }
+
+            Console.WriteLine();
+
+            var connectionStrings = ConfigurationManager.ConnectionStrings;
+
+            foreach (ConnectionStringSettings cnn in connectionStrings)
+            {
+                Console.WriteLine($"Name: { cnn.Name } Value: { cnn.ConnectionString }");
+            }
+
+            Console.WriteLine();
+
+            var appConfig = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            var appSettingsSection = appConfig.AppSettings.Settings;
+
+            string newKey = "Professor";
+            string newValue = "Fred James";
+
+            if (appSettingsSection[newKey] == null)
+            {
+                appSettingsSection.Add(newKey, newValue);
+            }
+            else
+            {
+                appSettingsSection[newKey].Value = newValue;
+            }
+
+            appConfig.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection(appConfig.AppSettings.SectionInformation.Name);
+
+            appSettings = ConfigurationManager.AppSettings;
+
+            foreach (var key in appSettings.AllKeys)
+            {
+                Console.WriteLine($"Key: { key } Value: { appSettings[key] }");
+            }
+            
+            Console.WriteLine();
+
+            Console.ReadLine();
         }
     }
 }
